4126279f3f4b75431240c70cefc70d6317bb8aed
Aravindh Rajkumar Jothi Subramanian <asubramanian@opengov.com>
2023-07-19 08:55:14 -0400

    modify to check

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 echo "Hello $1"
 time=$(date)
-# echo "time=$time" >> $GITHUB_OUTPUT
+echo "time=$time" >> $GITHUB_OUTPUT
 
-echo "##[set-output name=time]$time"
-echo "##[set-output name=person]$1"
+# echo "##[set-output name=time]$time"
+# echo "##[set-output name=person]$1"
